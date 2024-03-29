e8ce6ac9414a7b295268e413573b962f413604c4
rumaisaayaz <160394152+rumaisaayaz@users.noreply.github.com>
2024-03-30 01:30:43 +0500

    Add files via upload
    By the help of this Calculator you can do Basic Calculation

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstnumber" },
+    { message: "Enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "Select one of the operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Substraction", "Multiplication", "Divide"],
+    },
+]);
+// condtional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Divide") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please slelct valid operator");
+}
